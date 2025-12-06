f1a5f5c0f0795e616f18454c53283baa7cfea6b7
Moahmed Gharbawy <mohamed.gharbawy1610@gmail.com>
2025-12-06 22:11:53 +0200

    Componets Classes added

---
+++ Components/BUFF.h
@@ -0,0 +1,17 @@
+#pragma
+#ifndef _BUFF_H
+#define _BUFF_H
+#include "Gate.h"
+class BUFF : public Gate 
+{
+	public:
+	BUFF(const GraphicsInfo &r_GfxInfo);
+	virtual void Operate();	//Calculates the output according to the inputs
+	virtual void Draw(Output* pOut);	//for each component to Draw itself
+	virtual int GetOutPinStatus();	//returns status of outputpin if LED, return -1
+	virtual int GetInputPinStatus(int n);	//returns status of Inputpin # n if SWITCH, return -1
+	virtual void setInputPinStatus(int n, STATUS s);	//set status of Inputpin # n, to be used by connection class.
+	virtual ~BUFF();
+
+
+};
---
+++ Components/INV.cpp
@@ -0,0 +1,39 @@
+#include "INV.h"
+
+INV::INV(const GraphicsInfo& r_GfxInfo) :Gate(3, 5)
+{
+	m_GfxInfo.x1 = r_GfxInfo.x1;
+	m_GfxInfo.y1 = r_GfxInfo.y1;
+	m_GfxInfo.x2 = r_GfxInfo.x2;
+	m_GfxInfo.y2 = r_GfxInfo.y2;
+}
+
+void INV::Operate()
+{
+
+}
+// Function Draw	
+// Draws 3-input INV gate
+void INV::Draw(Output* pOut)
+{
+	//Call output class and pass gate drawing info to it.
+	pOut->DrawINV3(m_GfxInfo);
+}
+//returns status of outputpin
+int INV::GetOutPinStatus()
+{
+	return m_OutputPin.getStatus();
+}
+//returns status of Inputpin #n
+int INV::GetInputPinStatus(int n)
+{
+	return m_InputPins[n - 1].getStatus();	//n starts from 1 but array index starts from 0.
+}
+//Set status of an input pin ot HIGH or LOW
+void INV::setInputPinStatus(int n, STATUS s)
+{
+	m_InputPins[n - 1].setStatus(s);
+}
+INV3::~INV3()
+{
+}
