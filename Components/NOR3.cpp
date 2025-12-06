5af0f8530b31b77b111d8bf8dcd360fa25f9e18f
Orashy23 <omarorashy8@gmail.com>
2025-12-06 22:26:34 +0200

    testing NOR3

--- Components/NOR3.cpp
+++ Components/NOR3.cpp
@@ -1,1 +1,53 @@
 #include "NOR3.h"
+
+NOR3::NOR3(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(3, r_FanOut)
+{
+	m_GfxInfo.x1 = r_GfxInfo.x1;
+	m_GfxInfo.y1 = r_GfxInfo.y1;
+	m_GfxInfo.x2 = r_GfxInfo.x2;
+	m_GfxInfo.y2 = r_GfxInfo.y2;
+}
+
+
+void NOR3::Operate()
+{
+	//calculate the output status as the NORing of the three input pins
+	int highInputCount = 0;
+	for (int i = 1; i <= 3; i++)
+	{
+		if (GetInputPinStatus(i) == HIGH)
+		{
+			highInputCount++;
+		}
+	}
+	m_OutputPin.setStatus(highInputCount == 0 ? HIGH : LOW);
+}
+
+
+// Function Draw
+// Draws 3-input NOR gate
+void NOR3::Draw(Output* pOut)
+{
+	//Call output class and pass gate drawing info to it.
+	pOut->DrawNOR3(m_GfxInfo);
+}
+
+//returns status of outputpin
+int NOR3::GetOutPinStatus()
+{
+	return m_OutputPin.getStatus();
+}
+
+
+//returns status of Inputpin #n
+int NOR3::GetInputPinStatus(int n)
+{
+	return m_InputPins[n - 1].getStatus();	//n starts from 1 but array index starts from 0.
+}
+
+//Set status of an input pin to HIGH or LOW
+void NOR3::setInputPinStatus(int n, STATUS s)  // Updated function to match the NOR3 class
+{
+	m_InputPins[n - 1].setStatus(s);
+}
+
