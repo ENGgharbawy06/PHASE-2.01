a5dabc52f4f8eff6676bb559b3c9bf0fe4544ed8
Omar <Omar@LAPTOP-1E3639>
2025-12-12 21:43:23 +0200

    lesa bazabt el delete

---
+++ Actions/ActionDelete.h
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef ACTION_DELETE_H
+#define ACTION_DELETE_H
+
+#include "Action.h"
+#include "..\UI\UI.h"
+
+
+class ActionDelete : public Action
+{
+
+private:
+
+
+public:
+	ActionDelete(ApplicationManager* pApp);
+	void DeleteComponent();
+	virtual void Execute();
+
+};
+#endif
