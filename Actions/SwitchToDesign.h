e1813b6f8174aa34f11f8efdda05f715dfa8ad07
Mariamamr222 <mariamamrgomaa1@gmail.com>
2025-12-15 01:26:28 +0200

    SIM MODE

--- Actions/SwitchToDesign.h
+++ Actions/SwitchToDesign.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Action.h" // Assumed base class for all user commands
-#include "Actions/Action.h" // Alternative include path, use the correct one for your project
 
 // Class responsible for switching the application's operating mode
 // from Simulation Mode back to Design Mode.
@@ -25,5 +24,5 @@ public:
     virtual void Redo();
 
     // Virtual destructor is good practice for classes used polymorphically
-    virtual ~SwitchToDesign() {}
+    virtual ~SwitchToDesign();
 };
