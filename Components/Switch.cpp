b778b3c02192d6c8c9dca47742c1a18fe4799465
Orashy23 <omarorashy8@gmail.com>
2025-12-15 03:54:07 +0200

    ..

--- Components/Switch.cpp
+++ Components/Switch.cpp
@@ -1,1 +1,54 @@
 #include "Switch.h"
+
+Switch::Switch(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(0, r_FanOut)
+{
+	m_GfxInfo.x1 = r_GfxInfo.x1;
+	m_GfxInfo.y1 = r_GfxInfo.y1;
+	m_GfxInfo.x2 = r_GfxInfo.x2;
+	m_GfxInfo.y2 = r_GfxInfo.y2;
+
+	
+}
+
+void Switch::Operate()
+{
+	// Logic: If the switch is selected (toggled), it acts as ON (HIGH).
+	// Otherwise, it is OFF (LOW).
+	if (selected)
+		m_OutputPin.setStatus(HIGH);
+	else
+		m_OutputPin.setStatus(LOW);
+}
+
+void Switch::Draw(Output* pOut)
+{
+	// Call output class and pass switch drawing info and selection state
+	pOut->DrawSWITCH(m_GfxInfo, selected);
+}
+
+int Switch::GetOutPinStatus()
+{
+	return m_OutputPin.getStatus();
+}
+
+int Switch::GetInputPinStatus(int n)
+{
+	return -1; // Switch has no input pins
+}
+
+void Switch::setInputPinStatus(int n, STATUS s)
+{
+	// Switch has no input pins to set
+}
+
+Component* Switch::Clone(const GraphicsInfo& newGfx) const
+{
+	return new Switch(newGfx, SWITCH_FANOUT);
+}
+
+void Switch::SetGraphicsInfo(GraphicsInfo NewGfx)
+{
+	Component::SetGraphicsInfo(NewGfx);
+	m_GfxInfo = NewGfx;
+
+}
--- Components/Switch.h
+++ Components/Switch.h
@@ -1,5 +1,18 @@
 #pragma once
-class Switch
-{
-};
+#include "Gate.h"
 
+class Switch : public Gate
+{
+public:
+	Switch(const GraphicsInfo& r_GfxInfo, int r_FanOut);
+
+	virtual void Operate();	// Calculates the output according to the selection status
+	virtual void Draw(Output* pOut); // Draws the switch
+
+	virtual int GetOutPinStatus();	// returns status of outputpin
+	virtual int GetInputPinStatus(int n);	// returns -1 as switch has no inputs
+	virtual void setInputPinStatus(int n, STATUS s); // does nothing
+
+	virtual Component* Clone(const GraphicsInfo& newGfx) const;
+	virtual void SetGraphicsInfo(GraphicsInfo NewGfx);
+};
