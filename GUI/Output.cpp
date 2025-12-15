22923840b4ddbd5fff6319ad6636b178f5d4f4ae
Mo.Waleed <Mo.Waleed@LAIMONA>
2025-12-15 13:55:17 +0200

    ..

--- GUI/Output.cpp
+++ GUI/Output.cpp
@@ -177,7 +177,7 @@ void Output::DrawAND2(GraphicsInfo r_GfxInfo, bool sel) const
 
 }
 
-void Output ::GetValidDrawingPoint(int& x, int& y, Input* pIn) const //3lshan myrsmsh 3la el menu 
+void Output ::GetValidDrawingPoint(int& x, int& y, Input* pIn) const //3lshan myrsmsh 3la el menuu
 {
 	pIn->GetPointClicked(x, y);
 	while (y < UI.ToolBarHeight+35 || y > UI.height - UI.StatusBarHeight - UI.ToolBarHeight-35 || x < 0 || x > UI.width)
