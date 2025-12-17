fe57c900efc7c4d4d1d964157bf07989904f01c7
Orashy23 <omarorashy8@gmail.com>
2025-12-17 02:34:12 +0200

    ..

--- Actions/AddLED.h
+++ Actions/AddLED.h
@@ -10,6 +10,7 @@ private:
 	// Parameters for the rectangular area to be occupied by the LED
 	int Cx, Cy;	// Center point of the component
 	int x1, y1, x2, y2;	// Two corners of the rectangular area
+	LED* pLED;
 public:
 	AddLED(ApplicationManager* pApp);
 	virtual ~AddLED(void);
